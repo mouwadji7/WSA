18e0ac68ed6df7fe102c99d681d4b3f7255f45cb
mouwadji <mouwadji.learning@gmail.com>
2024-02-26 09:03:23 -0500

    soumission cote client rendu backend

--- Back_end/src/main/java/ca/mouwadji/WSA_testmongiste/controllers/SoumissionController.java
+++ Back_end/src/main/java/ca/mouwadji/WSA_testmongiste/controllers/SoumissionController.java
@@ -3,10 +3,31 @@ package ca.mouwadji.WSA_testmongiste.controllers;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ca.mouwadji.WSA_testmongiste.model.Soumission;
+import ca.mouwadji.WSA_testmongiste.service.SoumissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/Soumission")
 public class SoumissionController {
+
+    
+    private final SoumissionService soumissionService;
+
+    @Autowired
+    public SoumissionController(SoumissionService soumissionService) {
+        this.soumissionService = soumissionService;
+    }
+
+    @PostMapping("/soumission")
+    public ResponseEntity<Soumission> submitSoumission(@RequestBody Soumission soumission) {
+        Soumission savedSoumission = soumissionService.saveSoumission(soumission);
+        return new ResponseEntity<>(savedSoumission, HttpStatus.CREATED);
+    }
     
 }
--- Back_end/src/main/java/ca/mouwadji/WSA_testmongiste/service/SoumissionService.java
+++ Back_end/src/main/java/ca/mouwadji/WSA_testmongiste/service/SoumissionService.java
@@ -1,8 +1,21 @@
 package ca.mouwadji.WSA_testmongiste.service;
 
 import org.springframework.stereotype.Service;
+import ca.mouwadji.WSA_testmongiste.model.Soumission;
+import ca.mouwadji.WSA_testmongiste.repository.SoumissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class SoumissionService {
+
+    private final SoumissionRepository soumissionRepository;
     
+    @Autowired
+    public SoumissionService(SoumissionRepository soumissionRepository) {
+        this.soumissionRepository = soumissionRepository;
+    }
+
+    public Soumission saveSoumission(Soumission soumission) {
+        return soumissionRepository.save(soumission);
+    }
 }
