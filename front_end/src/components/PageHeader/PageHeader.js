d169671d444df6096999c2fffb862a9e4c2c21a6
mouwadji <mouwadji.learning@gmail.com>
2024-03-21 13:53:21 -0400

    debugg

--- front_end/src/components/PageHeader/PageHeader.js
+++ front_end/src/components/PageHeader/PageHeader.js
@@ -1,22 +1,23 @@
-// PageHeader.js
 import React from 'react';
 import './PageHeader.css';
+
+import { Link } from 'react-router-dom';
 
 function PageHeader({ onLogin }) { // Accepter la fonction de gestion de la connexion en tant que prop
   return (
     <header>
-      <nav class="navbar navbar-expand-sm bg-black navbar-dark fixed-top">
-        <div class="container">
+      <nav className="navbar navbar-expand-sm bg-black navbar-dark fixed-top">
+        <div className="container">
           <img src="/image/logo.png" alt="notre_logo"/>
-          <a class="navbar-brand" href="#">Home</a>
-          <a class="navbar-brand" href="#">Faire une soumission</a>
-          <a class="navbar-brand" href="#">État de ma soumission</a>
-          <a class="navbar-brand" href="#">Payer soumission</a>
-          <button type="button" class="btn btn-primary" onClick={onLogin}>Login</button> {/* Ajouter un gestionnaire d'événements au clic sur le bouton Login */}
+          <Link to="/" className="navbar-brand">Home</Link> {/* Fermez la balise Link correctement */}
+          <a className="navbar-brand">Faire une soumission</a>
+          <a className="navbar-brand" href="#">État de ma soumission</a>
+          <a className="navbar-brand" href="#">Payer soumission</a>
+          <button type="button" className="btn btn-primary" onClick={onLogin}>Login</button> {/* Ajouter un gestionnaire d'événements au clic sur le bouton Login */}
         </div>
       </nav>
     </header>
   );
 }
 
-export default PageHeader;
+export default PageHeader; // N'oubliez pas d'exporter le composant
