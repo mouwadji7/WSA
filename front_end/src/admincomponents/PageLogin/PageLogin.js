1759e8deafbfd421a6da0b1e145f01102fbecc88
mouwadji <mouwadji.learning@gmail.com>
2024-04-12 12:15:17 -0400

    modification de l'interface login

--- front_end/src/admincomponents/PageLogin/PageLogin.js
+++ front_end/src/admincomponents/PageLogin/PageLogin.js
@@ -1,85 +1,82 @@
-import {useState} from "react";
+import { useState } from "react";
+import { Alert, Button, Form } from "react-bootstrap";
 import axiosConfig from "../../axiosConfig";
-import {CONNECTED, setLogin as setUserLogged} from "../../login";
+import { CONNECTED, setLogin as setUserLogged } from "../../login";
 
+const NOT_EMPTY_ERROR_MESSAGE = "Le champ est requis";
 
+export default function LoginPage({ setLogin, setShowLoginPage }) {
+  const [message, setMessage] = useState("");
+  const [credentials, setCredentials] = useState({ username: "", password: "" });
+  const [inputsStatus, setInputsStatus] = useState({
+    username: { valid: false, errorMessage: "" },
+    password: { valid: false, errorMessage: "" }
+  });
 
-const NOT_EMPTY_ERROR_MESSAGE = "Le champs est requis"
-
-export default function ({setLogin, setShowLoginPage}) {
-    const [message, setMessage] = useState("");
-    const [credentials, setCredentials] = useState({username: "", password: ""});
-    const [inputsStatus, setInputsStatus] = useState({
-        username: { valid: false, errorMessage: "" },
-        password: { valid: false, errorMessage: "" }
-    });
-    const shouldDisabled = () => {
-        return !(inputsStatus.username.valid && inputsStatus.password.valid)
+  const handleUserNameChange = (e) => {
+    const value = e.target.value;
+    if (value === "") {
+      setInputsStatus((prevState) => ({ ...prevState, username: { valid: false, errorMessage: NOT_EMPTY_ERROR_MESSAGE } }));
+    } else {
+      setInputsStatus((prevState) => ({ ...prevState, username: { valid: true, errorMessage: "" } }));
     }
-    const handleUserNameChange = (e) => {
-        const value = e.target.value;
-        if (value === "") {
-            setInputsStatus( v => ( { ...v, username: {valid: false, errorMessage: NOT_EMPTY_ERROR_MESSAGE}}));
-        }else {
-            setInputsStatus( v => ( { ...v, username: {valid: true, errorMessage: ""}}));
-        }
-        setCredentials(v => ({...v, username: value}));
+    setCredentials((prevState) => ({ ...prevState, username: value }));
+  };
+
+  const handleUserPasswordChange = (e) => {
+    const value = e.target.value;
+    if (value === "") {
+      setInputsStatus((prevState) => ({ ...prevState, password: { valid: false, errorMessage: NOT_EMPTY_ERROR_MESSAGE } }));
+    } else {
+      setInputsStatus((prevState) => ({ ...prevState, password: { valid: true, errorMessage: "" } }));
     }
-    const handleUserPasswordChange = (e) => {
-        const value = e.target.value;
-        if (value === "") {
-            setInputsStatus( v => ( { ...v,  password: {valid: false, errorMessage: NOT_EMPTY_ERROR_MESSAGE}} ) )
-        }else {
-            setInputsStatus( v => ( { ...v,  password: {valid: true, errorMessage: ""}} ) )
-        }
-        setCredentials(v => ({...v, password: value}));
+    setCredentials((prevState) => ({ ...prevState, password: value }));
+  };
+
+  const handleAuthFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosConfig.post("/auth/generateToken", credentials);
+      setUserLogged(response.data);
+      setLogin({ status: CONNECTED });
+    } catch (error) {
+      setMessage("Vérifier vos informations de connexion");
+      console.error("Erreur lors de l'authentification:", error);
     }
+  };
 
-    const handleAuthFormSubmit = (e) => {
-        e.preventDefault();
-        getToken(credentials);
-    }
+  const shouldDisabled = () => {
+    return !(inputsStatus.username.valid && inputsStatus.password.valid);
+  };
 
-    const getToken = async (_credentials) => {
-        try {
-
-            const response = await axiosConfig.post(
-                "/auth/generateToken",
-                _credentials,
-            );
-            setUserLogged(response.data)
-            setLogin({status: CONNECTED})
-
-        } catch (error) {
-            setMessage("Vérifier vos informations de connexion")
-            console.error(
-                "Erreur lors de l'authentification:",
-                error,
-            );
-        }
-    };
-
-
-    return <section>
-        <p>{message}</p>
-        <form onSubmit={(e) => {handleAuthFormSubmit(e)}} >
-            <div>
-                <label>
-                    Nom d'utilisateur
-                    <input type={"text"} value={credentials.username} onChange={(e) =>  handleUserNameChange(e) } />
-                </label>
-
-                <p>{!inputsStatus.username.valid && inputsStatus.username.errorMessage}</p>
-            </div>
-            <div>
-                <label>
-                    Mot de passe
-                    <input type={"password"} value={credentials.password} onChange={(e) =>  handleUserPasswordChange(e) } />
-                </label>
-                <p>{!inputsStatus.password.valid && inputsStatus.password.errorMessage}</p>
-            </div>
-            <input disabled={shouldDisabled()} type={"submit"} value={"S'authentifier"} />
-            <input onClick={() => setShowLoginPage(false)} type={"button"} value={"Annuler"} />
-        </form>
-    </section>
+  return (
+    <div className="container d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
+      <section className="border p-4 text-center bg-black text-white">
+        <img src="/image/logo.png" alt="notre_logo"  className="mb-4" />
+        <h1>Bienvenue à nos employés !</h1>
+        <h2>Vous êtes sur le point de commencer votre travail.</h2>
+        <p>Vous avez tout le soutien dont vous avez besoin pour réussir.</p>
+        <p>Entrez vos informations de connexion pour commencer.</p>
+        {message && <Alert variant="danger">{message}</Alert>}
+        <Form onSubmit={(e) => handleAuthFormSubmit(e)}>
+          <Form.Group className="mb-3" controlId="username">
+            <Form.Label>Nom d'utilisateur</Form.Label>
+            <Form.Control type="text" value={credentials.username} onChange={handleUserNameChange} />
+            {!inputsStatus.username.valid && <Form.Text className="text-danger">{inputsStatus.username.errorMessage}</Form.Text>}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Mot de passe</Form.Label>
+            <Form.Control type="password" value={credentials.password} onChange={handleUserPasswordChange} />
+            {!inputsStatus.password.valid && <Form.Text className="text-danger">{inputsStatus.password.errorMessage}</Form.Text>}
+          </Form.Group>
+          <Button disabled={shouldDisabled()} variant="primary" type="submit">
+            S'authentifier
+          </Button>
+          <Button variant="secondary" onClick={() => setShowLoginPage(false)}>
+            Annuler
+          </Button>
+        </Form>
+      </section>
+    </div>
+  );
 }
